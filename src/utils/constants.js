fbe5103a0565a64b599d74798b1a4fc70b96f27f
Sankarsan Kampa <19631364+k3rn31p4nic@users.noreply.github.com>
2019-02-05 18:04:43 +0000

    utils: add some constants

---
+++ src/utils/constants.js
@@ -0,0 +1,16 @@
+export default {
+  fonts: {
+    sansSerif: "Roboto, sans-serif",
+    monospace: "Consolas, monospace",
+  },
+  colors: {
+    accent: "#ffd100",      // Yellow-ish
+    highlight: "#ff5800",   // Orange-ish
+    black: "#000000",
+    dark: "#111111",
+    darkGrey: "#aaaaaa",
+    lightGrey: "#bbbbbb",
+    light: "#f5f5f5",
+    white: "#ffffff",
+  }
+};
