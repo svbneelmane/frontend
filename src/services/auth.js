5ecab6a9e57ee8685a0e26aac16aa54bf1426376
Sankarsan Kampa <19631364+k3rn31p4nic@users.noreply.github.com>
2019-02-10 13:02:50 +0000

    services: add auth service

---
+++ src/services/auth.js
@@ -0,0 +1,34 @@
+export const isBrowser = () => typeof window !== "undefined";
+
+export const getUser = () =>
+  isBrowser() && window.localStorage.getItem("utsavUser")
+    ? JSON.parse(window.localStorage.getItem("utsavUser"))
+    : {};
+
+const setUser = user =>
+  window.localStorage.setItem("utsavUser", JSON.stringify(user));
+
+const authorize = ({ email, password }) => {
+  // TODO: Get user details from /users/login route.
+  // Send a GET request to API and check if the email & hashed password is
+  // present in the database.
+  // Return the user, if authorized. Otherwise, return {}.
+  return {};
+};
+
+export const handleLogin = (partialUser) => {
+  let user = authorize(partialUser);
+
+  return setUser(user);
+}
+
+export const isLoggedIn = () => {
+  const user = getUser();
+
+  return !!user.email;
+};
+
+export const logout = callback => {
+  setUser({});
+  callback();
+};
