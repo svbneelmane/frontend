96c8964d9f1c98b87de529e2fb71398c3e3506c1
Sankarsan Kampa <19631364+k3rn31p4nic@users.noreply.github.com>
2019-02-10 14:13:28 +0000

    pages: add app template page

---
+++ src/pages/app.jsx
@@ -0,0 +1,17 @@
+import React from "react";
+import { Router } from "@reach/router";
+
+import Layout from "../layouts/default";
+import PrivateRoute from "../components/PrivateRoute";
+import Login from "../components/Login";
+import Profile from "../components/Profile";
+
+export default () => (
+  <Layout>
+    <Router>
+      // TODO: Profile is a temporary page to test login.
+      <PrivateRoute path="/app" component={ Profile } />
+      <Login path="/app/login" />
+    </Router>
+  </Layout>
+);
