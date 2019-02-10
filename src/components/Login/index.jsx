7b1e07b1b808605561f4c24574ce33193e3cdabd
Sankarsan Kampa <19631364+k3rn31p4nic@users.noreply.github.com>
2019-02-10 13:56:33 +0000

    components: add Login component

---
+++ src/components/Login/index.jsx
@@ -0,0 +1,57 @@
+import React from "react";
+import { navigate } from "gatsby";
+import { handleLogin, isLoggedIn } from "../../services/auth";
+import {
+  Form, Icon, Input, Button,
+} from 'antd';
+
+class NormalLoginForm extends React.Component {
+  handleSubmit = event => {
+    event.preventDefault();
+
+    this.props.form.validateFields((err, values) => {
+      if (!err) handleLogin(values);
+    });
+  }
+
+  render() {
+    if (isLoggedIn()) navigate(`/app`);
+
+    const { getFieldDecorator } = this.props.form;
+    return (
+      <Form
+        onSubmit={event => {
+          this.handleSubmit(event);
+          navigate(`/app`);
+        }}
+        className="login-form"
+        style={{
+          margin: "0 auto",
+          maxWidth: 500
+        }}
+      >
+        <Form.Item>
+          {getFieldDecorator('email', {
+            rules: [{ required: true, type: "email", message: 'Please input your email!' }],
+          })(
+            <Input prefix={<Icon type="user" style={{ color: 'rgba(0,0,0,.25)' }} />} type="email" placeholder="Email" required />
+          )}
+        </Form.Item>
+        <Form.Item>
+          {getFieldDecorator('password', {
+            rules: [{ required: true, message: 'Please input your password!' }],
+          })(
+            <Input prefix={<Icon type="lock" style={{ color: 'rgba(0,0,0,.25)' }} />} type="password" placeholder="Password" required />
+          )}
+        </Form.Item>
+        <Form.Item>
+          <Button type="primary" htmlType="submit" className="login-form-button">
+            Log in
+          </Button>
+        </Form.Item>
+      </Form>
+    );
+  }
+}
+
+export default Form.create({ name: 'normal_login' })(NormalLoginForm);
