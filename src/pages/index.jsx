0a39948aa2204fa1d68aab48d4366e4bf1eb4345
Adarsh <adarshbhalotia2910@gmail.com>
2019-03-06 04:38:56 +0530

    Add redux elements

--- src/pages/index.jsx
+++ src/pages/index.jsx
@@ -1,5 +1,9 @@
 import React from "react";
+import { Provider } from 'react-redux'
+import configureStore from '../store';
+import App from '../App';
 
 import Layout from "../layouts/login";
 
-export default () => <Layout>Hello world!</Layout>;
+export default () => <Provider store={configureStore()}><Layout><App /></Layout></Provider>;
+
---
+++ src/reducers/rootReducer.js
@@ -0,0 +1,6 @@
+import { combineReducers } from 'redux';
+import reducerTheFirst from './reducerTheFirst';
+
+export default combineReducers({
+  reducerTheFirst,
+});
