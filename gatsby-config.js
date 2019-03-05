ddc1ec2c412aea9b2ea8ee3c6a4aa5a88bec252d
Traction <19631364+k3rn31p4nic@users.noreply.github.com>
2019-03-05 23:13:36 +0000

    add glamor

---
+++ gatsby-config.js
@@ -0,0 +1,10 @@
+module.exports = {
+  siteMetadata: {
+    title: "Manipal Utsav",
+    description: "Manipal Utsav 2019",
+    image: "",
+  },
+  plugins: [
+    "gatsby-plugin-glamor",
+  ],
+};
