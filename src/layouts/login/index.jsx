e3b9efec2289daabfdf3af411e4405c516967120
Traction <19631364+k3rn31p4nic@users.noreply.github.com>
2019-03-05 23:01:33 +0000

    fix paths

--- src/layouts/login/index.jsx
+++ src/layouts/login/index.jsx
@@ -1,8 +1,8 @@
 import React from "react";
 
-import Header from "../commons/Header";
-import Main from "../commons/Main";
-import Footer from "../commons/Footer";
+import Header from "../../commons/Header";
+import Main from "../../commons/Main";
+import Footer from "../../commons/Footer";
 
 import "../base.css";
 
