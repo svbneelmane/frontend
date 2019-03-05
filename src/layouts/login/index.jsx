29e3821af0f677efeecae4e3e7854268a73d530b
Traction <19631364+k3rn31p4nic@users.noreply.github.com>
2019-03-05 23:00:28 +0000

    layouts: update login layout

--- src/layouts/login/index.jsx
+++ src/layouts/login/index.jsx
@@ -1,9 +1,17 @@
 import React from "react";
 
+import Header from "../commons/Header";
+import Main from "../commons/Main";
+import Footer from "../commons/Footer";
+
 import "../base.css";
 
 export default (props) => (
   <>
-    { props.children }
+    <Header />
+    <Main>
+      { props.children }
+    </Main>
+    <Footer />
   </>
 );
