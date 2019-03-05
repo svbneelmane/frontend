0bb9ba0b5296fa01bceefe4720cec2fe03a52233
Meghashyam Bhandary <r21meghashyam@gmail.com>
2019-03-05 19:54:05 +0530

    added slot button

--- src/components/Cards/index.jsx
+++ src/components/Cards/index.jsx
@@ -1,13 +1,15 @@
 import React from 'react';
 import './style.css'
 import { Card, Tag, Col, Button } from 'antd';
+import { navigate } from 'gatsby';
 
 export const RoundCard = (props) => {
   return (
     <div >
       <Col  span={8}>
         <Card className="card" title={props.title} extra={<Tag color={props.tagColor}>{props.status}</Tag>}>
-          <Button onClick={ event => { props.setRoundId(props.id) }} className="start-btn" size="large" block type="primary"> Start </Button>
+          <Button onClick={ event => { navigate('/app/events/:eventid/') }} className="start-btn" size="large" block type="secondary"> Generate Slots </Button>
+          <Button onClick={ event => { props.setRoundId(props.id) }} className="start-btn" size="large" block type="primary"> Start Judging </Button>
         </Card>
       </Col>
   </div>
