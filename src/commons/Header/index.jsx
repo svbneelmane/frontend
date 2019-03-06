a458a51a648ec2bdc82df96e766247bcd2c10405
Traction <19631364+k3rn31p4nic@users.noreply.github.com>
2019-03-06 06:05:11 +0000

    commons: update Header component

--- src/commons/Header/index.jsx
+++ src/commons/Header/index.jsx
@@ -1,9 +1,63 @@
 import React from "react";
+import { Link } from "gatsby";
+
+import Logo from "../../../static/favicon.png";
+
+const HeaderLogo = () => (
+  <div css = {{
+    display: "flex",
+    alignItems: "center",
+  }}>
+    <div>
+      <img
+        src = { Logo }
+        height = "64"
+        css = {{
+          padding: 15,
+        }}
+      />
+    </div>
+    <div css = {{
+      fontSize: "1.3em",
+    }}>
+      Manipal Utsav
+    </div>
+  </div>
+);
+
+const HeaderLink = (props) => (
+  <li>
+    <Link to = { props.to } title = { props.title } css = {{
+      padding: "20px 10px",
+      color: "black",
+      ":hover": {
+        borderTop: "2px solid orange",
+      },
+    }}>
+      { props.title }
+    </Link>
+  </li>
+);
+const HeaderLinks = () => (
+  <ul css = {{
+    listStyle: "none",
+    display: "flex",
+    fontSize: "0.8em",
+  }}>
+    <HeaderLink title = "Events" to = "/events" />
+    <HeaderLink title = "Leaderboard" to = "/leaderboard" />
+  </ul>
+);
 
 export default () => (
   <header css = {{
+    display: "flex",
+    justifyContent: "space-between",
+    alignItems: "center",
     height: 64,
     boxShadow: "0 2px 8px #f0f1f2",
   }}>
+    <HeaderLogo />
+    <HeaderLinks />
   </header>
 );
