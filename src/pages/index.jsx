b12488deb581d4abdbcfe7475285e00770e6f200
Traction <19631364+k3rn31p4nic@users.noreply.github.com>
2019-03-06 00:05:55 +0000

    pages: update index page

--- src/pages/index.jsx
+++ src/pages/index.jsx
@@ -3,7 +3,11 @@ import { Provider } from 'react-redux'
 import configureStore from '../store';
 import App from '../App';
 
-import Layout from "../layouts/login";
+import Layout from "../layouts/app";
 
-export default () => <Provider store={configureStore()}><Layout><App /></Layout></Provider>;
-
+export default () => (
+  <Layout>
+    <div>s
+    </div>
+  </Layout>
+);
