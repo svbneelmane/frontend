f977706a664ab10c4d67c941a2ab4a4b7af546b4
Anil <aniljangir2010@gmail.com>
2019-03-06 06:56:18 +0530

    style for button and input box

--- src/commons/Form/index.jsx
+++ src/commons/Form/index.jsx
@@ -4,14 +4,24 @@ const Input = (props) => (
   <input
     type="text"
     name={props.name}
-    css={{width:400, height:20, padding:20, fontSize: "1em"}}
+    css={{width:400, 
+      height:20, 
+      padding:20, 
+      fontSize: "1em"}}
   />
 );
 
 const Button = (props) => (
   <button
     css={{
-      height:50, width:120, fontSize:"1em", background:"#ff5800", color:"#fff", borderRadius:"5px"
+      height:50, 
+      width:120, 
+      fontSize:"1em", 
+      background:"#ff5800", 
+      color:"#fff", 
+      borderRadius:"5px", 
+      ":focus":{outline:0}, 
+      ":hover":{ opacity:0.7}
     }}>
     {props.value}
   </button>
