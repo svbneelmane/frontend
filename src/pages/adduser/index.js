9746e9b8d401d580995f5a27fc48d401972a7256
Meghashyam Bhandary <r21meghashyam@gmail.com>
2019-03-08 23:33:48 +0530

    a

--- src/pages/adduser/index.js
+++ src/pages/adduser/index.js
@@ -7,7 +7,7 @@ export default class Adduser extends Component{
         return(
             <Layout>
                 <h3>Add User</h3>
-                <form class="form">
+                <form class="form" >
                     <div>
                         <label for="name">Name:</label>
                         <input name="name" autoComplete="off" id="name"/>
@@ -21,7 +21,29 @@ export default class Adduser extends Component{
                             <option>MCODS</option>
                         </datalist>
                     </div>
-                    
+                    <div>
+                        <label for="email">Email Id:</label>
+                        <input name="email" autoComplete="off" id="email"/>
+                       
+                    </div>
+                    <div>
+                        <label for="email">Mobile:</label>
+                        <input name="phone" autoComplete="off" id="mobile"/>
+                       
+                    </div>
+
+                    <div>
+                        <label for="type">Type:</label>
+                        <select>
+                            <option>Admin</option>
+                            <option>Faculty Coordinator</option>
+                            <option>Support Team</option>
+                        </select>
+                       
+                    </div>
+                    <div>
+                        <button>Create</button>
+                    </div>
                 </form>
             </Layout>
         );
