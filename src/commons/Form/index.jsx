8caf8452d4a6315ad3d9550074900667e1238f4c
Abid Hussain <abidhussain3008@gmail.com>
2019-03-08 18:22:13 +0530

    Add login View

--- src/commons/Form/index.jsx
+++ src/commons/Form/index.jsx
@@ -4,24 +4,44 @@ const Input = (props) => (
   <input
     type="text"
     name={props.name}
-    css={{width:400, 
-      height:20, 
-      padding:20, 
-      fontSize: "1em"}}
+    css={{
+      ...props.styles,
+      width: 400,
+      height: 20,
+      padding: 20,
+      fontSize: "1em"
+    }}
   />
 );
+
+const RounderInput = (props) => (
+  <input type={props.type} placeholder={props.placeholder} css={{
+    ...props.styles,
+    height: "32px",
+    width: "250px",
+    borderRadius: "3px",
+    marginBottom: 10,
+    border: "1px #efefef solid",
+    paddingLeft: "16px",
+    fontSize: "12px",
+    outline: "0",
+  }} />
+)
 
 const Button = (props) => (
   <button
     css={{
-      height:50, 
-      width:120, 
-      fontSize:"1em", 
-      background:"#ff5800", 
-      color:"#fff", 
-      borderRadius:"5px", 
-      ":focus":{outline:0}, 
-      ":hover":{ opacity:0.7}
+      ...props.styles,
+      position: "relative",
+      padding: "8px",
+      borderRadius: "3px",
+      backgroundColor: "#ff5800",
+      color: "white",
+      border: "none",
+      cursor: "pointer",
+      width: "100%",
+      ":focus": { outline: 0 },
+      ":hover": { opacity: 0.8 },
     }}>
     {props.value}
   </button>
@@ -29,5 +49,6 @@ const Button = (props) => (
 
 export {
   Input,
+  RounderInput,
   Button
 }
---
+++ src/components/Login/index.jsx
@@ -0,0 +1,33 @@
+import React from "react";
+import { RounderInput, Button } from "../../commons/Form";
+
+export default class Login extends React.Component {
+  render(){
+    return(
+      <div css={{
+        boxShadow: "0px 9px 12px -5px rgba(0, 0, 0, 0.1)",
+        padding: "16px 16px",
+        display: "block",
+        borderRadius: "5px",
+        position: "absolute",
+        left: "50%",
+        top: "50%",
+      }}>
+        <div css={{
+          textAlign: "center",
+          marginBottom: "16px"
+        }}>Lorem</div>
+        <div>
+          <RounderInput type="text" placeholder="Email" />
+          <br />
+          <RounderInput type="password" placeholder="Password" />
+        </div>
+        <div css={{
+          marginTop: "16px"
+        }}>
+          <Button styles={{ left : "50%", transform : "translateX(-50%)" }} onClick={() => { console.log("here") }} value="Sign in"></Button>
+        </div>
+      </div>
+    )
+  }
+}
