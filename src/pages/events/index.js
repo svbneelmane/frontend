90ac75396a6339053d14946850da55ed0f68ce18
Meghashyam Bhandary <r21meghashyam@gmail.com>
2019-03-09 02:09:42 +0530

    created eventCard

--- src/pages/events/index.js
+++ src/pages/events/index.js
@@ -4,36 +4,45 @@ import "./style.css";
 import dance from '../../images/dance.png'
 import singing from '../../images/singing.png'
 
-const EventCard=()=>{
+const EventCard=({event})=>(
+    <div className="event">
+    {console.log(event)}
+        <img src={event.image} alt={event.alt}/>
+        <div className="header">
+            <div className="name">{event.name}</div>
+            <div className="venue"><label>Venue:</label> {event.venue}</div>
+            <div className="time"><label>Date:</label> {event.date}</div>
+        </div>
+    </div>)
 
-}
 
 export default class Adduser extends Component{
+
+    events = [
+        {
+            image:dance,
+            alt:'dance',
+            name:'Western Solo Dancing',
+             venue: 'Manipal Institute of Technolody',
+             date: '2nd April'
+        },
+        {
+            image:singing,
+            alt:'singing',
+            name:'Solo Singing',
+             venue: 'Manipal College of Dental Sciences',
+             date: '2nd April'
+        }
+    ]
      
     render(){
         return(
             <Layout>
                 <h3>Events</h3>
                 <div className="events">
-                    <div className="event">
-                        <img src={dance} />
-                        <div className="header">
-                            <div className="name">Western Solo Dancing</div>
-                            <div className="venue"><label>Venue:</label> Manipal Institute of Technolody</div>
-                            <div className="time"><label>Date:</label> 2<sup>nd</sup> April</div>
-                        </div>
-                    </div>
-                    <div className="event">
-                        <img src={singing} />
-                        <div className="header">
-                            <div className="name">Solo Singing</div>
-                            <div className="venue"><label>Venue:</label> Manipal College of Dental Sciences</div>
-                            <div className="time">2<sup>nd</sup> April</div>
-                        </div>
-                    </div>
-                    
-                    
-                    
+                    {
+                        this.events.map(event=><EventCard event={event}/>)
+                    }
                 </div>
             </Layout>
         );
