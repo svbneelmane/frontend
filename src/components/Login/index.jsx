8938c7352d7da289651ec125f5d323ba2cb08757
Meghashyam Bhandary <r21meghashyam@gmail.com>
2019-03-09 14:18:53 +0530

    changed login styles

--- src/components/Login/index.jsx
+++ src/components/Login/index.jsx
@@ -2,6 +2,7 @@ import React from "react";
 import { Input, Button } from "../../commons/Form";
 import { connect } from 'react-redux';
 import { login } from '../../actions/userActions';
+import './style.css'
 
 const mapStateToProps = state => ({
   ...state,
@@ -34,28 +35,22 @@ class Login extends React.Component {
 
   render(){
     return(
-      <div css={{
-        boxShadow: "0px 9px 12px -5px rgba(0, 0, 0, 0.1)",
-        padding: "16px 16px",
-        display: "block",
-        borderRadius: "5px",
-        position: "absolute",
-        left: "50%",
-        top: "50%",
-      }}>
-        <div css={{
-          textAlign: "center",
-          marginBottom: "16px"
-        }}>Login</div>
-        <div>
-          <Input onChange={this.handleChange} name="email" type="text" placeholder="Email" />
-          <br />
-          <Input onChange={this.handleChange} type="password" name="password" placeholder="Password" />
-        </div>
-        <div css={{
-          marginTop: "16px"
-        }}>
-          <Button styles={{ left : "50%", transform : "translateX(-50%)" }} onClick={this.login} value="Sign in"></Button>
+      <div className="loginBoxContainer">
+        <div className="loginBox">
+          <div css={{
+            textAlign: "center",
+            marginBottom: "16px"
+          }}>Login</div>
+          <div>
+            <Input onChange={this.handleChange} name="email" type="text" placeholder="Email" />
+            <br />
+            <Input onChange={this.handleChange} type="password" name="password" placeholder="Password" />
+          </div>
+          <div css={{
+            marginTop: "16px"
+          }}>
+            <Button styles={{ left : "50%", transform : "translateX(-50%)" }} onClick={this.login} value="Sign in"></Button>
+          </div>
         </div>
       </div>
     )
