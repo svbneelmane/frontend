6cfb0af88447ece26fb3095963435f5f47e6a2e6
Abid Hussain <abidhussain3008@gmail.com>
2019-03-10 23:15:01 +0530

    add fetch events

--- src/pages/events/index.js
+++ src/pages/events/index.js
@@ -1,50 +1,69 @@
-import React,{ Component } from "react";
+import React, { Component } from "react";
 import Layout from '../../layouts/app';
 import "./style.css";
-import dance from '../../images/dance.png'
-import singing from '../../images/singing.png'
+import dance from '../../images/dance.png';
+import singing from '../../images/singing.png';
+import { get } from '../../actions/eventActions';
+import store from '../../reducers/commonReducer';
 
-const EventCard=({event})=>(
-    <div className="event">
-    {console.log(event)}
-        <img src={event.image} alt={event.alt}/>
-        <div className="header">
-            <div className="name">{event.name}</div>
-            <div className="venue"><label>Venue:</label> {event.venue}</div>
-            <div className="time"><label>Date:</label> {event.date}</div>
-        </div>
-    </div>)
+const EventCard = ({ event }) => (
+  <div className="event">
+    <img src={event.image} alt={event.alt} />
+    <div className="header">
+      <div className="name">{event.name}</div>
+      <div className="venue"><label>Venue:</label> {event.venue}</div>
+      <div className="time"><label>Date:</label> {event.date}</div>
+    </div>
+  </div>)
 
 
-export default class Adduser extends Component{
+export default class Adduser extends Component {
 
-    events = [
-        {
-            image:dance,
-            alt:'dance',
-            name:'Western Solo Dancing',
-             venue: 'Manipal Institute of Technolody',
-             date: '2nd April'
-        },
-        {
-            image:singing,
-            alt:'singing',
-            name:'Solo Singing',
-             venue: 'Manipal College of Dental Sciences',
-             date: '2nd April'
-        }
-    ]
-     
-    render(){
-        return(
-            <Layout>
-                <h3>Events</h3>
-                <div className="events">
-                    {
-                        this.events.map(event=><EventCard event={event}/>)
-                    }
-                </div>
-            </Layout>
-        );
+  constructor(props){
+    super(props)
+    this.state = {
+      events: {}
     }
+  }
+
+  events = [
+    {
+      image: dance,
+      alt: 'dance',
+      name: 'Western Solo Dancing',
+      venue: 'Manipal Institute of Technolody',
+      date: '2nd April'
+    },
+    {
+      image: singing,
+      alt: 'singing',
+      name: 'Solo Singing',
+      venue: 'Manipal College of Dental Sciences',
+      date: '2nd April'
+    }
+  ]
+
+  componentWillMount = () => {
+    get();
+    store.subscribe(async () => {
+      let state = await store.getState();
+      this.setState({
+        events: state.data,
+      })
+      console.log(this.state.events)
+    });
+  }
+
+  render() {
+    return (
+      <Layout>
+        <h3>Events</h3>
+        <div className="events">
+          {
+            this.events.map((event, i) => <EventCard key={i} event={event} />)
+          }
+        </div>
+      </Layout>
+    );
+  }
 }
