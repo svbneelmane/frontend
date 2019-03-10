043fa076c766fc46de6442aa791ed67237170861
Sankarsan Kampa <19631364+k3rn31p4nic@users.noreply.github.com>
2019-03-10 08:49:42 +0000

    pages: implement client side routes

--- src/pages/index.jsx
+++ src/pages/index.jsx
@@ -1,7 +1,24 @@
 import React from "react";
-import { Provider } from 'react-redux'
-import configureStore from '../store';
-// import App from '../App';
+import { Router } from "@reach/router";
+import { Provider } from "react-redux";
+
+import PrivateRoute from "../components/PrivateRoute";
+import Login from "../components/Login";
+import Profile from "../components/Profile";
+import NotFound from "../components/404";
+
+import configureStore from "../store";
 
 import Layout from "../layouts/app";
-export default () => <Provider store={configureStore()}><Layout></Layout></Provider>;
+
+export default () =>
+  <Provider store={ configureStore() }>
+    <Layout>
+      <Router>
+        <Login path="/login" />
+        <PrivateRoute path="/profile" component={ Profile } />
+        <NotFound path="/*" component={ NotFound } />
+      </Router>
+    </Layout>
+  </Provider>
+;
