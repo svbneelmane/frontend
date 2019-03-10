88cfd453875a44e6001701a389a892e511c3b539
Sankarsan Kampa <19631364+k3rn31p4nic@users.noreply.github.com>
2019-03-10 19:56:52 +0000

    pages: add private route to /collges/add

--- src/pages/index.jsx
+++ src/pages/index.jsx
@@ -5,6 +5,7 @@ import PrivateRoute from "../components/PrivateRoute";
 import Login from "../components/Login";
 import Profile from "../components/Profile";
 import Colleges from "../components/Colleges";
+import AddCollege from "../components/Colleges/Add";
 import NotFound from "../components/404";
 
 import configureStore from "../store";
@@ -17,6 +18,7 @@ export default () =>
         <Login path="/login" />
         <PrivateRoute path="/profile" component={ Profile } />
         <PrivateRoute path="/colleges" component={ Colleges } />
+        <PrivateRoute path="/colleges/add" component={ AddCollege } />
         <NotFound path="/*" component={ NotFound } />
       </Router>
     </Layout>
