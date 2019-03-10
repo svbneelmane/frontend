345287ec64167d45a9c4ae1f3813d4ffe103da67
Sankarsan Kampa <19631364+k3rn31p4nic@users.noreply.github.com>
2019-03-10 04:46:23 +0000

    commons(Form): UI improvements

--- src/commons/Form/index.jsx
+++ src/commons/Form/index.jsx
@@ -1,17 +1,16 @@
 import React from 'react';
 
 const Input = (props) => (
-  <input onChange={ (e) => props.onChange(e.target)} autoComplete="off" name={props.name} type={props.type} placeholder={props.placeholder} css={{
-    ...props.styles,
-    height: "32px",
-    width: "250px",
-    borderRadius: "3px",
-    marginBottom: 10,
-    border: "1px #efefef solid",
-    paddingLeft: "16px",
-    fontSize: "12px",
-    outline: "0",
-  }} />
+  <input
+    onChange = { (e) => props.onChange(e.target)}
+    autoComplete = "off"
+    name = {props.name}
+    type = {props.type}
+    placeholder = {props.placeholder}
+    css = {{
+      ...props.styles,
+    }}
+  />
 );
 
 const Tag = (props) => (
@@ -19,7 +18,7 @@ const Tag = (props) => (
     ...props.styles,
     display : "inline-block"
   }}>
-    {(props.type === 1) ? 
+    {(props.type === 1) ?
       <div css={{
         fontSize : "12px",
         backgroundColor: "#108ee9",
@@ -55,22 +54,12 @@ const Tag = (props) => (
 
 const Button = (props) => (
   <button
-    onClick={props.onClick}
-    css={{
+    css = {{
       ...props.styles,
-      position: "relative",
-      padding: "8px",
-      borderRadius: "3px",
-      backgroundColor: props.color ? props.color : "#ff5800",
-      color: props.fontColor ? props.fontColor : "white",
-      border: "none",
-      cursor: "pointer",
-      width: "100%",
-      fontSize: "16px",
-      ":focus": { outline: 0 },
-      ":hover": { opacity: 0.8 },
-    }}>
-    {props.value}
+    }}
+    onClick = {props.onClick}
+  >
+    {props.children}
   </button>
 );
 
