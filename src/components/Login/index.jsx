43f3fb6decab0b34920c9ccf2459b5225bff10dc
Sankarsan Kampa <19631364+k3rn31p4nic@users.noreply.github.com>
2019-03-10 04:16:50 +0000

    components(Login): ui changes

--- src/components/Login/index.jsx
+++ src/components/Login/index.jsx
@@ -1,11 +1,9 @@
 import React,{Component} from "react";
 import { Input, Button } from "../../commons/Form";
 import { login } from '../../actions/userActions';
-import './style.css'
 
 
 export default class Login extends Component {
-
   constructor(props) {
     super(props);
 
@@ -27,21 +25,29 @@ export default class Login extends Component {
 
   render(){
     return(
-      <div className="loginBoxContainer">
-        <div className="loginBox">
+      <div css = {{
+        width: "100%",
+        height: "100%",
+        display: "flex",
+        alignItems: "center",
+        justifyContent: "center",
+      }}>
+        <div css = {{
+          padding: 20,
+          borderRadius: 5,
+          boxShadow: "0 10px 50px -10px rgba(0, 0, 0, .2)",
+        }}>
           <div css={{
             textAlign: "center",
             marginBottom: "16px"
-          }}>Login</div>
+          }}>Login to your account</div>
           <div>
-            <Input onChange={this.handleChange} name="email" type="text" placeholder="Email" />
+            <Input onChange={this.handleChange} name="email" type="email" placeholder="Email" />
             <br />
             <Input onChange={this.handleChange} type="password" name="password" placeholder="Password" />
           </div>
-          <div css={{
-            marginTop: "16px"
-          }}>
-            <Button  onClick={this.login} value="Sign in"></Button>
+          <div>
+            <Button  onClick={this.login} value="Login"></Button>
           </div>
         </div>
       </div>
