95bf89b19bb02eefaf417fbc923b70846fd32ddc
Sankarsan Kampa <19631364+k3rn31p4nic@users.noreply.github.com>
2019-03-10 08:47:43 +0000

    pages: remove login page

--- src/pages/login.jsx
+++
@@ -1,8 +0,0 @@
-import React from "react";
-import { Provider } from 'react-redux'
-import configureStore from '../store';
-// import App from '../App';
-import Login from '../components/Login';
-
-import Layout from "../layouts/app";
-export default () => <Provider store={configureStore()}><Layout><Login /></Layout></Provider>;
