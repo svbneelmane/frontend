2af8457a88a7bcd29af929616aafef2683138d36
Sankarsan Kampa <19631364+k3rn31p4nic@users.noreply.github.com>
2019-03-10 05:25:13 +0000

    commons(Sidebar): improvements

--- src/commons/Sidebar/index.jsx
+++ src/commons/Sidebar/index.jsx
@@ -1,33 +1,72 @@
 import React, { Component } from "react";
 import store from "../../reducers/sidebarReducer";
-import './style.css';
 import { Link } from "gatsby";
-export default class Sidebar extends Component{
-    state={
-      open:false
-    }
-    componentDidMount(){
-      store.subscribe(()=>{
-        this.setState({open:store.getState()==='open'})
-      })
-    }
-    render=() => (
-    <sidebar className="sidebar" css = {{
+
+const SidebarItem = (props) => (
+  <li>
+    <Link to={ props.to } title={ props.title } css={{
       display: "block",
+      marginTop: 10,
+      padding: 10,
+      paddingLeft: 50,
+      fontSize: ".9em",
+      color: window.location.pathname === props.to ? "#ff5800" : "inherit",
+      backgroundColor: window.location.pathname === props.to ? "rgba(255, 209, 0, .2)" : "",
+      borderRight: "3px solid",
+      borderColor: window.location.pathname === props.to ? "#ff5800" : "white",
+      ":hover": {
+        color: "#ff5800",
+      },
+    }}>
+      { props.title }
+    </Link>
+  </li>
+);
+
+const SidebarItems = () => (
+  <ul css = {{
+    display: "flex",
+    flexDirection: "column",
+    margin: 0,
+    marginTop: 20,
+    listStyle: "none",
+    padding: 0,
+  }}>
+    <SidebarItem to="/" title="Home" />
+    <SidebarItem to="/events" title="Events" />
+    <SidebarItem to="/adduser" title="Add User" />
+    <SidebarItem to="/login" title="Login" />
+    <SidebarItem to="/profile" title="Profile" />
+  </ul>
+);
+
+export default class Sidebar extends Component {
+  state = {
+    open: false
+  };
+
+  componentDidMount(){
+    store.subscribe(() => {
+      this.setState({ open:store.getState() === "open" });
+    });
+  }
+
+  render = () => (
+    <div css = {{
+      display: "block",
+      height: "100vh",
+      overflow: "hidden",
       borderRight: "1px solid #efefef",
       minWidth: 300,
       marginLeft:this.state.open?0:-300,
       minHeight: "100vh",
-      transition: "margin-left 0.3s ease-out"
+      boxShadow: "25px 0px 50px -30px rgba(0, 0, 0, .1)",
+      transition: "margin-left 0.3s ease-out",
+      ":hover": {
+        overflowY: "auto",
+      },
     }}>
-      <ul>
-        <li><Link to="/">Home</Link></li>
-        <li><Link to="/adduser">Add User</Link></li>
-        <li><Link to="/login">Login</Link></li>
-        <li><Link to="/events">Events</Link></li>
-        <li><Link to="/profile">Profile</Link></li>
-        
-      </ul>
-    </sidebar>
+      <SidebarItems />
+    </div>
   );
 }
