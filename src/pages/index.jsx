b393a104bf08716870f9aadca7e3748347d93784
Sankarsan Kampa <19631364+k3rn31p4nic@users.noreply.github.com>
2019-03-12 06:41:51 +0000

    pages: add /judges route

--- src/pages/index.jsx
+++ src/pages/index.jsx
@@ -8,6 +8,7 @@ import Colleges from "../components/Colleges";
 import AddCollege from "../components/Colleges/Add";
 import Users from "../components/Users";
 import AddUser from "../components/Users/Add";
+import Judges from "../components/Judges";
 import NotFound from "../components/404";
 
 import configureStore from "../store";
@@ -20,11 +21,12 @@ export default () =>
         height: "100%",
       }}>
         <Login path="/login" />
+        <PrivateRoute path="/profile" component={ Profile } />
         <PrivateRoute path="/users" component={ Users } />
         <PrivateRoute path="/users/add" component={ AddUser } />
-        <PrivateRoute path="/profile" component={ Profile } />
         <PrivateRoute path="/colleges" component={ Colleges } />
         <PrivateRoute path="/colleges/add" component={ AddCollege } />
+        <PrivateRoute path="/judges" component={ Judges } />
         <NotFound path="/*" component={ NotFound } />
       </Router>
     </Layout>
