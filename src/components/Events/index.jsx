bf01bc3ff3c37355cc9f4040021d86d7560d9973
Sankarsan Kampa <19631364+k3rn31p4nic@users.noreply.github.com>
2019-03-13 09:11:01 +0000

    components: minor changes to Events view

--- src/components/Events/index.jsx
+++ src/components/Events/index.jsx
@@ -1,75 +1,100 @@
 import React from "react";
 import { Link } from "gatsby";
 
-// import dance from '../../images/dance.png';
-// import singing from '../../images/singing.png';
-import eventIcon from '../../images/event.png';
-import {get} from '../../services/eventService';
+import eventIcon from "../../images/event.png";
+import { get } from "../../services/eventService";
 import { toast } from "../../actions/toastActions";
 
 const EventCard = ({ event }) => (
-  <div className="event">
-    <img src={event.image} style={{borderRadius:"50%"}} alt={event.alt} />
-    <div className="header">
-      <div className="name">{event.name}</div>
-      <div className="venue"><label>Venue:</label> {event.venue}</div>
-      <div className="time"><label>Date:</label> {event.date}</div>
+  <Link to={ "/events/" + event.id } css={{
+    color: "inherit",
+  }}>
+    <div css={{
+      padding: "15px 0",
+      display: "flex",
+      alignItems: "center",
+      borderBottom: "1px dashed rgba(0, 0, 0, .1)",
+    }}>
+      <div css={{
+        marginRight: 20,
+      }}>
+        <img
+          src={ event.image }
+          alt={ event.name }
+        />
+      </div>
+      <div>
+        <div css={{
+          fontSize: "1.3em",
+        }}>
+          { event.name }
+        </div>
+        <div css={{
+          color: "rgba(0, 0, 0, .5)",
+        }}>
+          { event.description }
+        </div>
+        <div css={{
+          fontSize: "0.9em",
+          color: "green",
+        }}>
+          { /* TODO: If we know the time the event starts, we should use .toLocaleString() instead. */ }
+          starts {(new Date(event.startDate)).toDateString()} at { (event.venue ? event.venue + " - " : "") + event.college.name }
+        </div>
+        <div css={{
+          fontSize: "0.8em",
+          color: "rgba(0, 0, 0, .5)",
+        }}>
+          { event.rounds.length } Round{ event.rounds.length === 1 ? "" : "s" }
+        </div>
+      </div>
     </div>
-  </div>)
+  </Link>
+);
 
 export default class Events extends React.Component {
-  constructor(props){
-    super(props)
+  constructor(props) {
+    super(props);
+
     this.state = {
-      events: []
-    }
+      events: [],
+    };
   }
 
-  /*events = [
-    {
-      image: dance,
-      alt: 'dance',
-      name: 'Western Solo Dancing',
-      venue: 'Manipal Institute of Technolody',
-      date: '2nd April'
-    },
-    {
-      image: singing,
-      alt: 'singing',
-      name: 'Solo Singing',
-      venue: 'Manipal College of Dental Sciences',
-      date: '2nd April'
-    }
-  ]*/
+  componentWillMount = async () => {
+    let response = await get();
 
-  componentWillMount = async() => {
-   let response = await get();
-   if(!response)
-    return toast("Failed to load events, refresh to try again. ");
-  if(response.status!==200)
-    return toast(response.message);
-    console.log(response.data);
-  let events = response.data.map(event=>{
-    return{
-      image:eventIcon,
-      alt:'event',
-      venue:event.venue,
-      date:(new Date(event.startDate)).toDateString(),
-      name:event.name,
-    }
-  })
-  this.setState({events})
-  }
+    if (!response) return toast("Failed to load events, refresh to try again.");
+    if (response.status !== 200) return toast(response.message);
+
+    let events = response.data.map(event=>({
+      id: event.id,
+      name: event.name,
+      description: event.description || "This is a fantastic event!",
+      college: event.college,
+      venue: event.venue,
+      rounds: event.rounds,
+      startDate: event.startDate,
+      endDate: event.endDate,
+
+      image: eventIcon,
+    }));
+
+    this.setState({ events });
+  };
 
   render = () => (
     <div>
-      <h2>Events <Link to="/events/add"><button>Add Event</button></Link></h2>
-      <p>Events of MUCAPP.</p>
-      <div className="events">
-          {
-            this.state.events.map((event, i) => <EventCard key={i} event={event} />)
-          }
-        </div>
+      <div>
+        <h2>Events</h2>
+        <p>Events in Utsav.</p>
+        <Link to="/events/add"><button>Add Event</button></Link>
+      </div>
+      <div css={{
+        marginTop: 20,
+      }}>
+        { this.state.events.map((event, i) => <EventCard key={i} event={event} />) }
+      </div>
     </div>
   );
 };
