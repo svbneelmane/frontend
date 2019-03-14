4dc59ffee541d9ec4b7c7479821bfb20b8917ebd
Traction <19631364+k3rn31p4nic@users.noreply.github.com>
2019-03-14 04:40:43 +0000

    views: minor changes to registration

--- src/components/Registration/index.jsx
+++ src/components/Registration/index.jsx
@@ -2,7 +2,6 @@ import React from "react";
 import { Link } from "gatsby";
 
 import eventsService from "../../services/events";
-import { toast } from "../../actions/toastActions";
 
 const EventCard = ({ event }) => (
   <Link to={ "/register/" + event.id } css={{
