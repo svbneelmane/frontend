c3d99e77bb71b6b424de4b185ec27cf2aa0afcc9
Sankarsan Kampa <19631364+k3rn31p4nic@users.noreply.github.com>
2019-03-15 01:56:53 +0000

    add logout confirmation

--- src/components/Profile/index.jsx
+++ src/components/Profile/index.jsx
@@ -7,11 +7,25 @@ import constants from "../../utils/constants";
 import avatar from "../../images/user.svg";
 
 export default class Profile extends React.Component {
+  state = {
+    logoutClicks: 0,
+  };
+
   handleLogout() {
-    logout(() => {
-      navigate("/");
-      return null;
-    });
+    if (!this.state.logoutClicks) {
+      this.setState({
+        logoutClicks: this.state.logoutClicks + 1,
+      });
+    } else {
+      this.setState({
+        logoutClicks: this.state.logoutClicks - 1,
+      });
+
+      logout(() => {
+        navigate("/");
+        return null;
+      });
+    }
   }
 
   render() {
@@ -30,7 +44,7 @@ export default class Profile extends React.Component {
           <p css={{ color: "rgba(0, 0, 0, .7)" }}>{ data.email || "..." }</p>
           <p css={{ color: "rgba(0, 0, 0, .5)" }}>{ data.type ? constants.getUserType(data.type) : "..." }</p>
         </div>
-        <button onClick={ () => this.handleLogout() }>Logout</button>
+        <button onClick={ () => this.handleLogout() }>{ this.state.logoutClicks ? "Sure?" : "Logout" }</button>
       </div>
     );
   }
