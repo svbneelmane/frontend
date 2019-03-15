1d23983d9c6ca7bbd78d1cb3c8e4fb9e734a34ac
Sankarsan Kampa <19631364+k3rn31p4nic@users.noreply.github.com>
2019-03-15 19:54:48 +0000

    components: improvements to registered team view

--- src/components/Teams/index.jsx
+++ src/components/Teams/index.jsx
@@ -1,21 +1,59 @@
 import React from "react";
+import { Link } from "gatsby";
 
-import reducer from "../../reducers/commonReducer";
-import { getTeamsForCollege } from "../../services/collegeServices";
-import { TeamList } from "../../commons/List";
+import collegesService from "../../services/colleges";
 
+const styles = {
+  teamCard: {
+    display: "inline-block",
+    marginRight: 20,
+    marginBottom: 20,
+    padding: 20,
+    width: 250,
+    borderRadius: 3,
+    border: "2px solid rgba(0, 0, 0, .1)",
+    color: "inherit",
+    boxShadow: "0px 5px 20px -4px rgba(0, 0, 0, .1)",
+    transition: "box-shadow .2s ease",
+    ":hover": {
+      color: "inherit",
+      boxShadow: "0px 5px 50px -4px rgba(0, 0, 0, .1)",
+    }
+  },
+};
+
+const TeamCard = (props) => (
+  <Link to={ "/colleges/" + props.college + "/teams" } css={{
+    ...styles.teamCard,
+  }}>
+    <div>{ props.name }</div>
+    <div css={{
+      fontSize: ".7em",
+      color: "grey",
+    }}>
+      { props.members + " member" + (props.members === 1 ? "" : "s") }
+    </div>
+  </Link>
+);
 
 export default class Teams extends React.Component {
   state = {
-    teams: [],
+    events: [],
+    teams: {},
   };
 
   componentWillMount() {
-    getTeamsForCollege(this.props.college);
+    collegesService.getTeams(this.props.college).then(teams => {
+      let sortedTeams = {};
+      let events = Array.from(new Set(teams.map(team => team.event.name)));
 
-    reducer.subscribe(() => {
-      reducer.getState().then(state => {
-        this.setState({ teams: state.data.list });
+      for (let event of events) {
+        sortedTeams[event] = teams.filter(team => team.event.name === event);
+      }
+
+      this.setState({
+        events,
+        teams: sortedTeams,
       });
     });
   }
@@ -23,18 +61,35 @@ export default class Teams extends React.Component {
   render = () => {
     return (
       <div>
-        <h2>Teams</h2>
         <div>
-        {
-          this.state.teams.map((team, i) => (
-              <TeamList
-                key={ i }
-                number={i+1}
-                name={team.name}
-                subName={team.event.name}
-              />
-            ))
-        }
+          <h2>Teams</h2>
+        </div>
+        <div>
+          {
+            this.state.events.length
+            ? this.state.events.map((event, i) => (
+                <div key={ i }>
+                  <div>
+                    <h3>{ event }</h3>
+                  </div>
+                  <div>
+                    {
+                      this.state.teams[event].map((team, i) => (
+                        <TeamCard
+                          key={ i }
+                          college = { this.props.college }
+                          name = { team.name }
+                          members = { team.members.length }
+                        />
+                      ))
+                    }
+                  </div>
+                </div>
+              ))
+            : <div>
+              No teams have been registered yet.
+            </div>
+          }
         </div>
       </div>
     );
