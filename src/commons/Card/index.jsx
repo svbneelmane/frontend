0a1e119d34660189951de0d7069c8a2518acdb57
Sankarsan Kampa <19631364+k3rn31p4nic@users.noreply.github.com>
2019-03-20 07:37:43 +0530

    commons(Card): add delete button to RoundCard

--- src/commons/Card/index.jsx
+++ src/commons/Card/index.jsx
@@ -1,37 +1,44 @@
 import React from 'react';
 import { Button, Input } from '../Form'
 import { navigate } from 'gatsby';
+import { FiX } from "react-icons/fi";
 
 const RoundCard = (props) => (
   <div
     css={{
       margin: "2%",
+      padding: 20,
       borderRadius: "4px",
       // border: "2px solid #efefef",
       width: "29%",
       display: "inline-block",
       boxShadow: "0px 5px 12px -5px rgba(0, 0, 0, .1)",
     }}
-  > 
-    <div
-      css={{
-        padding: "16px",
-        fontSize: "1.2em",
-        display: "inline-block"
-      }}
-    >{props.title} </div>
+  >
+    <div css={{
+      display: "flex",
+      justifyContent: "space-between",
+      marginBottom: 20,
+      fontSize: "1.2em",
+    }}>
+      <span>{ props.title }</span>
+      <span css={{
+        cursor: "pointer",
+        ":hover": {
+          color: "red",
+        },
+      }}>
+        <FiX onClick={ () => props.onClick(props.roundId) } />
+      </span>
+    </div>
 
       {/* TODO: enable tag later on*/}
       {/*<Tag styles={{
               float: "right",
               margin: 16,
             }} type={props.type}/>*/}
-    <div
-    css={{
-      padding: "16px",
-    }}
-    >
-      <Button onClick={ event => { navigate(`/events/${props.eventId}/rounds/${props.roundId}/slot`) }} styles={{color:"black", backgroundColor : "#f5f5f5", width: "100%"}}  color="#f5f5f5" value="View Slots" >View Slots</Button> 
+    <div>
+      <Button onClick={ event => { navigate(`/events/${props.eventId}/rounds/${props.roundId}/slot`) }} styles={{color:"black", backgroundColor : "#f5f5f5", width: "100%"}}  color="#f5f5f5" value="View Slots" >View Slots</Button>
       <Button onClick={ event => { navigate(`/judge/${props.eventId}/rounds/${props.roundId}`); }} styles={{marginTop: "16px", width: "100%"}} value="Start Round" >Start Round</Button>
     </div>
   </div>
@@ -46,7 +53,7 @@ const CriteriaCard = (props) => (
       display: "inline-block",
       boxShadow: "0px 5px 12px -5px rgba(0, 0, 0, .1)",
     }}
-  > 
+  >
     <div
       css={{
         padding: "16px",
@@ -60,7 +67,7 @@ const CriteriaCard = (props) => (
       padding: "16px",
     }}
     >
-      <Input type="number" 
+      <Input type="number"
       onChange={props.onChange}
       styles={{
         width: "100%",
